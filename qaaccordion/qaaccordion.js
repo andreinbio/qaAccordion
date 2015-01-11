0c965b1a66aceaa5ad50f791e729c2cced96d4b6
Andrei Nagacevschi <andrein_bio@yahoo.com>
2015-01-11 17:17:15 +0200

    updated .js and .css files

--- qaaccordion/qaaccordion.js
+++ qaaccordion/qaaccordion.js
@@ -3,15 +3,21 @@
 		
 		//default settings
 		var settings = $.extend({
-			qa_wrapper: "#qaAccordion",
-			qa_item: ".qa_item",
 			question_container: ".qa_question",
 			answer_container: ".qa_answer",
-			slide_speed: "fast"
+			slide_speed: "fast",
+			fa_path:"qaaccordion/font-awesome-4.2.0/css/font-awesome.min.css"
 		}, options);
 		
+		loadCSS = function(href) {
+			var cssLink = $("<link rel='stylesheet' type='text/css' href='"+href+"'>");
+			$("head").append(cssLink); 
+		};
+		
+		loadCSS(settings.fa_path);
+		
 		//the function ...
-		this.find(settings.question_container).click(function () {
+		this.find(settings.question_container).append("<i class='fa fa-plus'></i>").click(function () {
 			if ($(this).hasClass("qa_on")) {
 				
 				$(this).next(settings.answer_container).slideToggle(settings.slide_speed, function () {
